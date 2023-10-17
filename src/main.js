7743e34be63acec9b376da89bb754f58f7b41470
Jacky Chen <jchen577@ucsc.edu>
2023-10-16 22:05:58 -0700

    finished game

--- src/main.js
+++ src/main.js
@@ -5,4 +5,6 @@ let config = {
     scene: [Menu,Play]
 }
 let game = new Phaser.Game(config);
-
+let borderUISize = game.config.height/15;
+let borderPadding = borderUISize/3;
+let keyF,keyR,keyLEFT,keyRIGHT;
---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,21 @@
+class Spaceship extends Phaser.GameObjects.Sprite{
+    constructor(scene,x,y,texture,frame,pointValue){
+        super(scene,x,y,texture,frame);
+    
+        scene.add.existing(this);//Add objects to a scene that already exists
+        this.points = pointValue;
+        this.moveSpeed = game.settings.spaceshipSpeed;
+    }
+    
+    update(){
+        this.x -= this.moveSpeed;
+
+        if(this.x <= 0 -this.width){
+            this.reset();
+        }
+    }
+
+    reset(){
+        this.x = game.config.width;
+    }
+}
